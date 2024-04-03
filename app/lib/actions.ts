f18c8b73bd18b40ab476d25fa86d888caf636c1c
admin <admin@admindeMacBook-Pro.local>
2024-04-03 13:52:07 +0800

    server invalidation on edit invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,7 +14,7 @@ const FormSchema = z.object({
         .gt(0, { message: 'Please enter an amount greater than $0.' }),
     status: z.enum(['pending', 'paid'], {
         invalid_type_error: 'Please select an invoice status.',
-      }),
+    }),
     date: z.string(),
 });
 
@@ -23,9 +23,9 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 // This is temporary until @types/react-dom is updated
 export type State = {
     errors?: {
-      customerId?: string[];
-      amount?: string[];
-      status?: string[];
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
     };
     message?: string | null;
 };
@@ -76,13 +76,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+    id: string,
+    prevState: State,
+    formData: FormData,
+) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
